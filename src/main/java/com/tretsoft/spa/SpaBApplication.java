16e59e09c536c396b9d34360114a5124d4691b12
Alwertus <alwertus@gmail.com>
2022-11-06 14:57:12 +0300

    initial commit

---
+++ src/main/java/com/tretsoft/spa/SpaBApplication.java
@@ -0,0 +1,13 @@
+package com.tretsoft.spa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpaBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tretsoft/spa/SpaBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tretsoft.spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
