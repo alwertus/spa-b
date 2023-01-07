2d58033e3f937bc028d5e4527996651cfd774f08
Alwertus <alwertus@gmail.com>
2023-01-08 01:35:34 +0600

    configuration

--- src/main/java/com/tretsoft/spa/SpaApplication.java
+++ src/main/java/com/tretsoft/spa/SpaApplication.java
@@ -5,13 +5,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Properties;
 
 @Log4j2
 @SpringBootApplication
 public class SpaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+
+        // determine config path
+        String configPath = "file:" + System.getProperty("user.home") + "/apps/config/spab.yml";
+        System.getProperties().setProperty("spring.config.location", configPath);
+
         SpringApplication.run(SpaApplication.class, args);
+        log.info("Load config file: " + configPath);
         log.info("===== ===== SPA-B Application is started ===== ====");
     }
 
